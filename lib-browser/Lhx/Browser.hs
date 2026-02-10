0cf11262d6bdd4d186715f0078287bcc098c8d5f
astynax <astynax@users.noreply.github.com>
2026-02-10 10:13:18 +0400

    make browser spawning more cross-platform(ish)

--- lib-browser/Lhx/Browser.hs
+++ lib-browser/Lhx/Browser.hs
@@ -11,7 +11,13 @@ withBrowserOnFreePort serveOn = do
   port <- getFreePort
   void $ forkIO do
     waitWith mempty 100000 "127.0.0.1" port
-    void . spawnCommand $ "xdg-open http://127.0.0.1:" <> show port
+    let addr = "http://127.0.0.1:" <> show port
+    let cmd =
+          unwords
+          [ "xdg-open", addr, ">", "/dev/null"
+          , "||"
+          , "open", addr, ">", "/dev/null" ]
+    void $ spawnCommand cmd
   serveOn port
 
 withFreePort :: (Int -> IO ()) -> IO ()
